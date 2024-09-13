18e792bc150b2ae71fdfc2319b82652392ebb2fb
unknown <kshitizdubey2011@gmail.com>
2024-09-14 00:52:06 +0530

    PathLab

--- Nirog/src/main/java/com/PathLab/Nirog/Repository/PatientRepository.java
+++ Nirog/src/main/java/com/PathLab/Nirog/Repository/PatientRepository.java
@@ -3,5 +3,8 @@ package com.PathLab.Nirog.Repository;
 import com.PathLab.Nirog.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findById(Long id);
 }
--- Nirog/src/main/java/com/PathLab/Nirog/Service/PatientService.java
+++ Nirog/src/main/java/com/PathLab/Nirog/Service/PatientService.java
@@ -2,6 +2,7 @@ package com.PathLab.Nirog.Service;
 
 import com.PathLab.Nirog.Patient;
 import com.PathLab.Nirog.Repository.PatientRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +15,17 @@ public class PatientService {
         @Autowired
         private PatientRepository patientRepository;
 
+    @Cacheable("patients")
+    public Optional<Patient> getPatientById(Long id) {
+        return patientRepository.findById(id);
+    }
+
         public Patient savePatient(Patient patient) {
             return patientRepository.save(patient);
         }
 
         public List<Patient> getAllPatients() {
             return patientRepository.findAll();
-        }
-
-        public Optional<Patient> getPatientById(Long id) {
-            return patientRepository.findById(id);
         }
 
         public void deletePatient(Long id) {
